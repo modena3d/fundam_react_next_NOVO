15aa2c2ed8b0057505a95999c254cb630f759935
EmpresaFict <modena3d@gmail.com>
2025-10-26 19:15:16 -0300

    correto

---
+++ src/app/basico/jsx/page.tsx
@@ -0,0 +1,10 @@
+export default function ExemploJSX() {
+  return (
+    <div>
+      <h1>Exemplo de JSX</h1>
+      <div>
+        <span>Teste</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/basico/primeiro/page.tsx
@@ -0,0 +1,19 @@
+export default function PrimeiroComponente() {
+  return (
+    <div className="flex flex-col gap-3 p-8">
+      <div>
+        <h1 className="text-3xl font-black">Primeiro Componente</h1>
+        <p className="text-sm text-zinc-400">Subtítulo da nossa página</p>
+      </div>
+      <div>
+        <span>Conteúdo</span>
+      </div>
+      <button
+        className="bg-blue-500 px-6 py-2
+      rounded-lg hover:bg-blue-600 self-start"
+      >
+        Salvar
+      </button>
+    </div>
+  );
+}
