e5ee64007ea46fd87538c1ed5b197c4f84078bfd
EmpresaFict <modena3d@gmail.com>
2025-11-01 10:44:19 -0300

    rhrh

--- src/components/Aprovados.tsx
+++ src/components/Aprovados.tsx
@@ -1,11 +1,15 @@
-export default function Aprovado(props: any) {
-    return (
-        <div className="flex justify-between bg-black p-3 rounded-md">
-            <span className="text-white">{props.nome} ---</span>
-            <div className="flex gap-2">
-                <button className="bg-blue-500 p-2 text-sm rounded-md">Editar</button>
-                <button className="bg-red-500 p-2 text-sm rounded-md">Excluir</button>
-            </div>
-        </div>
-    )
+export interface AprovadoProps {
+  nome: string;
+}
+
+export default function Aprovado(props: AprovadoProps) {
+  return (
+    <div className="flex justify-between bg-black p-3 rounded-md">
+      <span className="text-white">{props.nome} ---</span>
+      <div className="flex gap-2">
+        <button className="bg-blue-500 p-2 text-sm rounded-md">Editar</button>
+        <button className="bg-red-500 p-2 text-sm rounded-md">Excluir</button>
+      </div>
+    </div>
+  );
 }
